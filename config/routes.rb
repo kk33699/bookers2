7eebfcdb48f8ff07261d6a9933c4168870481df7
kk33699 <183782474+kk33699@users.noreply.github.com>
2024-12-09 13:30:27 +0000

    ～データベース上に外部キーを追加_20241209

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+
+  get 'homes/top' => 'homes#top'
   devise_for :users
+  resources :books
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
