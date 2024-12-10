f0f39d774c08127e356815ad4168f6060ff8363c
kk33699 <183782474+kk33699@users.noreply.github.com>
2024-12-10 08:00:19 +0000

    ログイン名をNameに変更_20241210

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :books, dependent: :destroy
+         validates :name, presence: true, uniqueness: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+  root to: 'homes#top'
   get 'homes/top' => 'homes#top'
   devise_for :users
   resources :books
