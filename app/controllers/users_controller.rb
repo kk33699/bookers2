acbf0b25fdd09af8ece99cd2a53b2f9d9ddf5d2d
kk33699 <183782474+kk33699@users.noreply.github.com>
2024-12-12 11:37:45 +0000

    User infoのプロフィール編集を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def index
     @users = User.all
   end
   
-    def show
+  def show
     @user = User.find(params[:id])  
     @books = @user.books.page(params[:page])
   end
 
   def edit
+    
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Profile was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image) 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :books, dependent: :destroy
-         validates :name, presence: true, uniqueness: true
-         has_one_attached :image
+  has_many :books, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+
+  has_one_attached :image
+
+  validates :introduction,  length: { maximum: 500 }, allow_blank: true
 
 end
