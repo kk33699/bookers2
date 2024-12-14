2ed71147e7b2f5e82610e42d312c098f3f5b2a38
kk33699 <183782474+kk33699@users.noreply.github.com>
2024-12-14 04:43:01 +0000

    showの大枠作成、indexの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,14 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books = Book.all
+    @books = Book.includes(:user)
     @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @books = Book.where(user_id: @user.id)
   end
 
   def edit
@@ -33,6 +35,15 @@ class BooksController < ApplicationController
       redirect_to books_path, notice: 'Book was successfully deleted.' # 削除成功後にリダイレクト
     else
       redirect_to books_path, alert: 'Failed to delete the book.' # 削除に失敗した場合の処理
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id]) # 編集対象のBookを取得
+    if @book.update(book_params) # フォームから送信されたデータで更新
+      redirect_to book_path(@book), notice: 'Book was successfully updated.' # 更新成功時にリダイレクト
+    else
+      render :edit # 更新失敗時に編集画面を再表示
     end
   end
 
