3f89114d6bec244ad32515cb8d7d58ab63e0ecde
kk33699 <183782474+kk33699@users.noreply.github.com>
2024-12-15 18:29:28 +0000

    ベッターの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'homes/top' => 'homes#top'
-  get 'homes/about', to: 'homes#about', as: :about
+  get 'homes/about' => 'homes#about'
   devise_for :users
   resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :users
